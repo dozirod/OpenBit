b9dd9abefc2c1e7712948ad2c90c246440d07e74
Nicoleta Dozirodovich <lisiy8636@icloud.com>
2026-03-06 17:03:16 +0200

    Create index.php

---
+++ pages/software/index.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+
+require dirname(__DIR__, 2) . '/includes/catalog.php';
+$page = (int)($_GET['page'] ?? 2);
+if ($page < 2) {
+    $page = 2;
+}
+
+openbit_render_catalog_page('software', $page, '../../');
